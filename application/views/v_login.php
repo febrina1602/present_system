8f21a8e9e1f04f84df3bac7ed548368940c5dfa0
febrina1602 <112796775+febrina1602@users.noreply.github.com>
2022-10-09 14:43:42 +0700

    First commit

---
+++ application/views/v_login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/bootstrap-4.6.2/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 mt-2">
+                
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/bootstrap-4.6.2/css/bootstrap.min.css"></script>
+</body>
+</html>
